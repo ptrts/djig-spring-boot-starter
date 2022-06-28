dd06f876d7d3fe12acb28cdecc73987a26af74c4
Pavel Taruts <ptrtss@gmail.com>
2022-06-28 16:51:57 +0300

    -

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("java-library")
+    id("maven-publish")
 }
 
 group = "org.taruts.dynamic-java-code-stored-in-git"
@@ -31,7 +32,7 @@ dependencies {
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
 
-    implementation("we.git-implementations:dynamic-api:001")
+    implementation("org.taruts.dynamic-java-code-stored-in-git:core-dynamic-api:001")
 }
 
 tasks.named<Test>("test") {
@@ -41,5 +42,42 @@ tasks.named<Test>("test") {
 dependencyManagement {
     imports {
         mavenBom("org.springframework.boot:spring-boot-dependencies:2.7.0")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("jar") {
+            // We use the java-library plugin in this project. The java-library is based upon the java plugin.
+            // During the build process, the java plugin creates a so-called component which is a collection of things to publish.
+            // The maven-publish plugin can create publications from components.
+            // that the maven-publish can use. The component is named "java" after the java plugin.
+            from(components["java"])
+
+            // Also we use the plugin io.spring.dependency-management.
+            // This plugin enables us not to specify versions manually for those dependencies of the project
+            // that Spring libraries work with.
+            // But by default the dependency versions in the java component are those specified manually.
+            // This configuration is needed to change this default.
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionResult()
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            name = "s3MavenRepo"
+            url = uri("s3://maven.taruts.net")
+            authentication {
+                // AwsImAuthentication means that the credentials are in an AWS profile on the computer
+                // Only the author of this project has those credentials and can upload dynamic-api artifacts
+                register("aws", AwsImAuthentication::class)
+            }
+        }
     }
 }
